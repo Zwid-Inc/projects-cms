ca320528a4ed54b826dd9bf78840b0e9e54a8967
chrisitstyle <infernos1244@o2.pl>
2024-10-20 15:01:05 +0200

    Initialize spring boot project with basic dependencies

---
+++ server/src/main/java/com/projectscms/server/ProjectsContentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.projectscms.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsContentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsContentManagementSystemApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/projectscms/server/ProjectsContentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectscms.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsContentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

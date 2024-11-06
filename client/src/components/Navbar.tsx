ba482f592aa09ed0b5665e96b89b5f3b759c3621
Ikar <ikarpiatkowski@proton.me>
2024-11-06 21:09:48 +0100

    refactor sidebar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,16 @@
 "use client";
-import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
+import { useSidebar } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { ThemeToggle } from "./ThemeToggle";
 
 export const Navbar = () => {
   const { open } = useSidebar();
@@ -9,12 +19,29 @@ export const Navbar = () => {
     <nav
       className={cn(
         "fixed top-0 left-0 w-full p-4 backdrop-blur-[6px] z-10 transition-all duration-300",
-        open ? "pl-[260px]" : "pl-0" // Adjust padding-left based on sidebar state
+        open ? "pl-[260px]" : "pl-[48px]" // Adjust padding-left based on sidebar state
       )}
     >
       <div className="flex w-full justify-between items-center">
         <div className="flex items-center">
-          <SidebarTrigger />
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">
+                      Building Your Application
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
           <a href="/">
             <h1 className="text-xl font-bold ml-2">Stock Tracker</h1>
           </a>
@@ -22,7 +49,9 @@ export const Navbar = () => {
         <div className="flex justify-center flex-1">
           <p className="font-bold">Profits: 267.83 PLN</p>
         </div>
-        <div>X</div>
+        <div>
+          <ThemeToggle />
+        </div>
       </div>
     </nav>
   );

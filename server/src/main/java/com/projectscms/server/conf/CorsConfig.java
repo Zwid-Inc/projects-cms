6314e19d4e6de83c1b421d261908019bab68e350
PawelPloszaj <41387367+PawelPloszaj@users.noreply.github.com>
2024-12-05 17:19:38 +0100

    Quick Fix

--- server/src/main/java/com/projectscms/server/conf/CorsConfig.java
+++ server/src/main/java/com/projectscms/server/conf/CorsConfig.java
@@ -1,29 +1,28 @@
-// package com.projectscms.server.conf;
+package com.projectscms.server.conf;
 
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class CorsConfig {
-//     @Value("${CLIENT_URL}")
-//     private String CLIENT_URL;
+@Configuration
+public class CorsConfig {
+    @Value("${CLIENT_URL}")
+    private String CLIENT_URL;
 
-//     @Bean
-//     public WebMvcConfigurer corsConfigurer() {
-
-//         return new WebMvcConfigurer() {
-//             @Override
-//             public void addCorsMappings(CorsRegistry registry) {
-//                 registry.addMapping("/**")
-//                         .allowedOrigins(CLIENT_URL)
-//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                         .allowedHeaders("Content-Type", "Authorization")
-//                         .exposedHeaders("Authorization", "Content-Type")
-//                         .maxAge(3600); // cache preflight request
-//             }
-//         };
-//     }
-// }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(CLIENT_URL)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Content-Type", "Authorization")
+                        .exposedHeaders("Authorization", "Content-Type")
+                        .maxAge(3600); // cache preflight request
+            }
+        };
+    }
+}
